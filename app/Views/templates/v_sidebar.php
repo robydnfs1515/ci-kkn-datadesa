32516d7757cb5fe2e8a6bf7551788932b350e767
robydnfs1515 <novri.170170120@mhs.unimal.ac.id>
2022-12-29 20:59:53 +0700

    update

---
+++ app/Views/templates/v_sidebar.php
@@ -0,0 +1,107 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+  <!-- Sidebar - Brand -->
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <div class="sidebar-brand-icon rotate-n-15">
+      <i class="fas fa-laugh-wink"></i>
+    </div>
+    <div class="sidebar-brand-text mx-3">Data Desa <sup>Tambon Tunong</sup></div>
+  </a>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider my-0">
+
+  <!-- Nav Item - Dashboard -->
+  <li class="nav-item">
+    <a class="nav-link" href="index.html">
+      <i class="fab fa-dashcube"></i>
+      <span>Dashboard</span></a>
+  </li>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider">
+
+  <!-- Heading -->
+  <div class="sidebar-heading">
+    Menu
+  </div>
+
+  <!-- Nav Item - Pages Collapse Menu -->
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#">
+      <i class="fas fa-tablets"></i>
+      <span>BLT Subsidi Upah</span>
+    </a>
+  </li>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider d-none d-md-block">
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#">
+      <i class="fas fa-pills"></i>
+      <span>BLT BBM</span>
+    </a>
+  </li>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider d-none d-md-block">
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#">
+      <i class="fas fa-wine-glass"></i>
+      <span>Program Kartu Prakerja</span>
+    </a>
+  </li>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider d-none d-md-block">
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#">
+      <i class="fas fa-window-restore"></i>
+      <span>BLT UMKM</span>
+    </a>
+  </li>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider d-none d-md-block">
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#">
+      <i class="fas fa-vote-yea"></i>
+      <span>Bantuan Pokok Non Tunai</span>
+    </a>
+  </li>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider d-none d-md-block">
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#">
+      <i class="fas fa-viruses"></i>
+      <span>Bansos PBI JK</span>
+    </a>
+  </li>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider d-none d-md-block">
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#">
+      <i class="fas fa-vest-patches"></i>
+      <span>BLT Dana Desa</span>
+    </a>
+  </li>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider d-none d-md-block">
+
+  <!-- Sidebar Toggler (Sidebar) -->
+  <div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+  </div>
+
+</ul>
+<!-- End of Sidebar -->
